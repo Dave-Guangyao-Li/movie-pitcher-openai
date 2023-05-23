aeaf338cbfe09a8c35a656f86356dfc219d0a762
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-05-23 10:10:22 -0700

    fetch bot reply to movie idea

--- index.js
+++ index.js
@@ -13,16 +13,35 @@ const openai = new OpenAIApi(configuration)
 document.getElementById("send-btn").addEventListener("click", () => {
   const setupTextarea = document.getElementById('setup-textarea')
   if (setupTextarea.value) {
+    const userInput = setupTextarea.value
     setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
+    fetchBotReply()
+    // fetchSynopsis(userInput)
   }
-  fetchBotReply()
+
 })
 
-async function fetchBotReply() {
+async function fetchBotReply(outline) {
   const response = await openai.createCompletion({
-    'model': 'text-davinci-003',
-    'prompt': 'Sound enthusiastic in five words or less.'
+    model: 'text-davinci-003',
+    prompt: `Generate a short message to enthusiastically say an outline sounds interesting and that you need some minutes to think about it.
+    ###
+    outline: Two dogs fall in love and move to Hawaii to learn to surf.
+    message: I'll need to think about that. But your idea is amazing! I love the bit about Hawaii!
+    ###
+    outline: A plane crashes in the jungle and the passengers have to walk 1000km to safety.
+    message: I'll spend a few moments considering that. But I love your idea!! A disaster movie in the jungle!
+    ###
+    outline: A group of corrupt lawyers try to send an innocent woman to jail.
+    message: Wow that is awesome! Corrupt lawyers, huh? Give me a few moments to think!
+    ###
+    outline: ${outline}
+    message: 
+    `,
+    max_tokens: 60
   })
   movieBossText.innerText = response.data.choices[0].text.trim()
 }
+
+async function fetchSynopsis(userInput) {
